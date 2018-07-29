a55458bee04da0b896710551d39c9458911484b5
jun097kim <jun097kim@gmail.com>
2018-07-29 10:23:45 +0900

    미리보기 모달 구현

--- web/src/components/ContractCardList/ContractCardList.js
+++ web/src/components/ContractCardList/ContractCardList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -8,6 +8,7 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import "./ContractCardList.scss";
+import PreviewBox from "../PreviewBox";
 
 const styles = {
   card: {
@@ -34,37 +35,58 @@ const contracts = [
  *
  * @param props 부모 컴포넌트로부터 넘겨받는 값
  */
-const ContractCardList = props => {
-  const { classes } = props;
+class ContractCardList extends Component {
+  state = {
+    previewBox: false // 모달 상태
+  };
 
-  return (
-    <div className="ContractCardList">
-      <Grid container spacing={24}>
-        {contracts.map((contract, index) => {
-          return (
-            <Grid item xs={12} sm={6} key={index}>
-              <Card className={classes.card}>
-                <CardContent>
-                  <Typography gutterBottom variant="headline" component="h2">
-                    {contract.title}
-                  </Typography>
-                  <Typography component="p">{contract.subtitle}</Typography>
-                </CardContent>
-                <CardActions>
-                  <Button component={Link} to="#" color="primary">
-                    미리보기
-                  </Button>
-                  <Button component={Link} to={contract.link} color="primary">
-                    작성하기
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          );
-        })}
-      </Grid>
-    </div>
-  );
-};
+  /**
+   * 모달 토글
+   */
+  handleTogglePreviewBox = () => {
+    this.setState({ previewBox: !this.state.previewBox });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { previewBox } = this.state;
+
+    return (
+      <div className="ContractCardList">
+        <Grid container spacing={24}>
+          {contracts.map((contract, index) => {
+            return (
+              <Grid item xs={12} sm={6} key={index}>
+                <Card className={classes.card}>
+                  <CardContent>
+                    <Typography gutterBottom variant="headline" component="h2">
+                      {contract.title}
+                    </Typography>
+                    <Typography component="p">{contract.subtitle}</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      component={Link}
+                      to="#"
+                      color="primary"
+                      onClick={this.handleTogglePreviewBox}
+                    >
+                      미리보기
+                    </Button>
+                    <Button component={Link} to={contract.link} color="primary">
+                      작성하기
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+        {/*모달 상태가 true일 때만 렌더링*/}
+        {previewBox && <PreviewBox onClose={this.handleTogglePreviewBox} />}
+      </div>
+    );
+  }
+}
 
 export default withStyles(styles)(ContractCardList);
---
+++ web/src/components/PreviewBox/PreviewBox.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import CloseIcon from "react-icons/lib/md/close";
+import "./PreviewBox.scss";
+
+class PreviewBox extends Component {
+  render() {
+    const { onClose } = this.props;
+
+    return (
+      <div className="PreviewBox">
+        <div className="dark" onClick={onClose} />
+        <div className="modal">
+          <div className="head">
+            <h3>부동산매매계약서</h3>
+            <div className="exit" onClick={onClose}>
+              <CloseIcon />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PreviewBox;
