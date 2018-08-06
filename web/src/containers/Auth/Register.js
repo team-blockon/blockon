e84aadacc6cc01be07678bedf773ed499abf7f77
jun097kim <jun097kim@gmail.com>
2018-08-07 02:40:11 +0900

    accounts 컬렉션에 중개인 여부 필드 추가

--- web/src/containers/Auth/Register.js
+++ web/src/containers/Auth/Register.js
@@ -3,13 +3,15 @@ import AuthContent from 'components/AuthContent';
 import InputWithLabel from 'components/InputWithLabel';
 import AuthButton from 'components/AuthButton';
 import * as AuthAPI from 'lib/api/auth';
+import { Tabs, Tab } from '@material-ui/core';
 
 class Register extends Component {
   state = {
     username: '',
     email: '',
     password: '',
-    passwordCheck: ''
+    passwordCheck: '',
+    isJunggae: 0
   };
 
   handleChange = event => {
@@ -21,11 +23,17 @@ class Register extends Component {
     });
   };
 
+  handleTabChange = (event, value) => {
+    this.setState({
+      isJunggae: value
+    });
+  };
+
   handleRegister = event => {
     const { history } = this.props;
-    const { username, email, password } = this.state;
+    const { username, email, password, isJunggae } = this.state;
 
-    AuthAPI.register({ username, email, password }).then(() => {
+    AuthAPI.register({ username, email, password, isJunggae }).then(() => {
       history.push('/');
     });
   };
@@ -38,8 +46,15 @@ class Register extends Component {
   };
 
   render() {
+    const { isJunggae } = this.state;
+
     return (
       <AuthContent title="회원가입">
+        <Tabs value={isJunggae} onChange={this.handleTabChange}>
+          <Tab label="매도 / 매수인" />
+          <Tab label="공인중개사" />
+        </Tabs>
+
         <InputWithLabel
           label="이름"
           type="text"
@@ -73,7 +88,10 @@ class Register extends Component {
           onChange={this.handleChange}
           onKeyPress={this.handleKeyPress}
         />
-        <AuthButton onClick={this.handleSubmit}>회원가입</AuthButton>
+
+        {isJunggae === 1 && <div>중개인 인증</div>}
+
+        <AuthButton onClick={this.handleRegister}>회원가입</AuthButton>
       </AuthContent>
     );
   }
