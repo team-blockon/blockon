71fc6802217845ea647975c3bd114bb9edeaf448
comso <>
2018-08-17 01:51:31 +0900

    마이페이지 접속시 비밀번호 검사 삭제
    - 회원가입시 비밀번호를 입력하지 않기에 비밀번호 검사로직 삭제

--- web/src/server/routes/api/mypage/mypage.controller.js
+++ web/src/server/routes/api/mypage/mypage.controller.js
@@ -6,51 +6,6 @@ const nodemailer = require("nodemailer");
 const randomstring = require("randomstring");
 
 /**
- * 회원정보에 접속하기 위해 패스워드 확인
- * @param req : token : 현재 로그인 된 아이디의 토큰, password: 회원정보에 접속 하기위한 패스워드 확인값
- * @param res
- */
-exports.authPassword = (req, res) => {
-    const { token, password } = req.body;
-
-    const correctPassword = (tokenPassword, inputPassword) => {
-        return new Promise((resolve, reject) => {
-            if(tokenPassword !== inputPassword){
-                reject(new Error("wrong password"));
-            }else{
-                resolve(true);
-            }
-        });
-    };
-
-    const respond = () => {
-        res.json({
-            result : true,
-            message: "registered successfully"
-        });
-    };
-
-    // 에러가 있을 때 실행되는 함수 (이미 존재하는 email)
-    const onError = error => {
-        res.status(409).json({
-            result : false,
-            message: error.message
-        });
-    };
-
-    TokenUtil.decodeToken(token)
-        .then( (decodedToken) => {
-            return Account.findByEmail(decodedToken.email);
-        })
-        .then((account) => {
-            let passwordHash = CryptoUtil.hashing(password);
-            return correctPassword(account.password, passwordHash);
-        })
-        .then(respond)
-        .catch(onError);
-};
-
-/**
  * 인증 email 전송
  * @param req
  * @param res
