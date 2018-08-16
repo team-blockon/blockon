040cd2f263c44bd986f27c53d8157728a5b08eb9
comso <>
2018-08-17 01:50:29 +0900

    공인중개사무소 검색을 위한 정보 저장및 조회
    - 같은 이름의 공인중개사무소가 있기 때문에 id(auto increment) 추가 예정
      - 사용자에게는 주소로 구분 할수있게 제공해야 하기 때문에 주소도 함께저장
    
    - 공인중개사무소 저장은 인증시에 목록 저장 하거나 api 사용하면 되지 않을까..?

---
+++ web/src/server/routes/api/agent/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const { find, save } = require("./agent.controller");
+
+router.get("/", find);
+router.post("/", save);
+
+
+module.exports = router;
--- web/src/server/routes/api/index.js
+++ web/src/server/routes/api/index.js
@@ -6,6 +6,7 @@ const user = require('./user');
 const contract = require("./contract");
 const mypage = require("./mypage");
 const qna = require("./qna");
+const agent = require("./agent");
 
 api.use('/pdf', pdf);
 api.use('/auth', auth);
@@ -15,5 +16,6 @@ api.use('/user', user);
 api.use("/contract", contract);
 api.use("/mypage", mypage);
 api.use("/qna", qna);
+api.use("/agent", agent);
 
 module.exports = api;
