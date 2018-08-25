711321e2962619f7513647f59d454cddb6231fda
jun097kim <jun097kim@gmail.com>
2018-08-25 12:15:10 +0900

    스크롤 되었을 경우, fixed navbar 색상 변경

--- web/src/components/Header/Header.js
+++ web/src/components/Header/Header.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import classNames from 'classnames';
 import Logo from 'static/images/logo-header.png';
 import LogoBlack from 'static/images/logo-header-black.png';
 import './Header.scss';
 
 class Header extends Component {
+  state = {
+    scrolled: false // 스크롤 여부
+  };
+
+  handleScroll = () => {
+    const header = document.querySelector('header');
+
+    if (window.scrollY > header.clientHeight) {
+      // 헤더 높이보다 더 스크롤이 되었을 경우
+      this.setState({
+        scrolled: true
+      });
+    } else {
+      this.setState({
+        scrolled: false
+      });
+    }
+  };
+
+  componentDidMount() {
+    // 스크롤 이벤트 리스너 설정
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    // 스크롤 이벤트 리스너 해제
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
   render() {
+    const { scrolled } = this.state;
     const { navItem, location } = this.props;
 
     return (
-      <header className={location.pathname === '/' && 'landing'}>
+      <header
+        className={classNames({
+          /* / 라우트이면서 스크롤이 되지 않은 경우에만 landing 클래스 추가 */
+          landing: location.pathname === '/' && !scrolled
+        })}
+      >
         <div className="menu container">
           <Link to="/">
             <img
-              src={location.pathname === '/' ? Logo : LogoBlack}
+              src={location.pathname === '/' && !scrolled ? Logo : LogoBlack}
               className="logo"
               alt="logo"
             />
