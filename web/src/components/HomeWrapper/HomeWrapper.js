25e1ba4c9ea89d954005c976a01bf454f1e8f79a
zerone2 <yicho93@naver.com>
2018-08-27 12:03:21 +0900

    main text change

--- web/src/components/HomeWrapper/HomeWrapper.js
+++ web/src/components/HomeWrapper/HomeWrapper.js
@@ -23,7 +23,7 @@ const BlockonSection = () => {
       <img src={logo} className="logo" alt="logo" />
       <p>
         BLOCKON은 부동산 계약을 더욱 투명하게 하기 위한 블록체인 기반&nbsp;
-        '공인중개사 평판' 시스템입니다.
+        '부동산 거래 관리' 시스템입니다.
       </p>
       <p>
         공인중개사 검색은 더욱 쉽게, 거래과정을 명확하게, 소통은 원활하게
@@ -274,7 +274,7 @@ class HomeWrapper extends Component {
           <div className="logo">
             <img src={coverLogo} alt="logo" />
           </div>
-          <div className="subtitle">믿을 수 있는 중개인을 만나보세요</div>
+          <div className="subtitle">부동산 사기 위험에서 벗어나세요</div>
           <div className="search">
             <AutoComplete dataSource={dataSource} onSearch={this.handleSearch}>
               <Input
