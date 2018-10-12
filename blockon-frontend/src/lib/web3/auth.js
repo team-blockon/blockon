e7641496350f9b2fd7308b533e86faa1b66d5cf6
himchan kim <kimhim7025@gmail.com>
2018-10-13 00:43:28 +0900

    createAccount 함수 호출 인자 변경
    Blockon 스마트 컨트랙트의 수정으로 인한 변경

--- blockon-frontend/src/lib/web3/auth.js
+++ blockon-frontend/src/lib/web3/auth.js
@@ -2,25 +2,21 @@ import { openNotification } from 'lib/utils';
 
 /**
  * Account 생성
- * @param {Object} { ethAddress, email }
+ * @param {*} ethAddress
  */
-export const createAccount = ({ ethAddress, email }) => {
+export const createAccount = ethAddress => {
   return new Promise((resolve, reject) => {
     const { blockon } = window;
 
     try {
       // Blockon 계약 내의 createAccount 함수 호출
-      blockon.createAccount.sendTransaction(
-        ethAddress,
-        email,
-        (error, txHash) => {
-          if (!error) {
-            resolve({ txHash });
-          } else {
-            reject({ msg: error });
-          }
+      blockon.createAccount.sendTransaction(ethAddress, (error, txHash) => {
+        if (!error) {
+          resolve({ txHash });
+        } else {
+          reject({ msg: error });
         }
-      );
+      });
     } catch (e) {
       if (e.message === 'invalid address') {
         openNotification('잠시 후 다시 시도해 주세요.');
