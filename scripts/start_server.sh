0a3bec49e3ad2f3f3663f7d8a3b2b4fade09876a
jun097kim <jun097kim@gmail.com>
2018-10-22 04:10:18 +0900

    docker-compose: command not found 에러 해결

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
-docker-compose -f docker-compose-deploy.yaml up -d
+/usr/local/bin/docker-compose -f docker-compose-deploy.yaml up -d
