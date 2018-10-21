a1cfe6ad9abd21319ac42f2044bfb0c87fa2a4e0
jun097kim <jun097kim@gmail.com>
2018-10-22 04:46:14 +0900

    docker-compose 실행 전 컨테이너 중지 및 삭제

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
+/usr/local/bin/docker-compose -f docker-compose-deploy.yaml down
 /usr/local/bin/docker-compose -f docker-compose-deploy.yaml up -d
