be263a61f8ded0d24b364b7c2ef0edd273c9ad79
jun097kim <jun097kim@gmail.com>
2018-10-22 03:26:40 +0900

    Change directory to deployment-archive in shell script.

--- scripts/load_image.sh
+++ scripts/load_image.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+cd deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 docker load -i blockon_express.tar
 docker load -i blockon_nginx.tar
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+cd deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 docker-compose -f docker-compose-deploy.yaml up -d
