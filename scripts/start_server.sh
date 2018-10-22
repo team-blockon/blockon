13bd3dfe272151ac9c0c648a4bd0fef16f7c7e9e
jun097kim <jun097kim@gmail.com>
2018-10-22 11:59:35 +0900

    서버 시작 전 도커 컨테이너 강제 종료
    - is already in use by container 에러 해결

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cd deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 /usr/local/bin/docker-compose -f docker-compose-deploy.yaml down
+docker rm -f express
+docker rm -f nginx
 /usr/local/bin/docker-compose -f docker-compose-deploy.yaml up -d
