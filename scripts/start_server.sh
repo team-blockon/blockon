5dbd81757076695c35c9a55abd454f274816aa60
jun097kim <jun097kim@gmail.com>
2018-10-23 21:38:41 +0900

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cd deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
+docker rm -f blockon_express
+docker rm -f blockon_nginx
 /usr/local/bin/docker-compose -f docker-compose-deploy.yaml down
 /usr/local/bin/docker-compose -f docker-compose-deploy.yaml up -d
