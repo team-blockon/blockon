c1cd7f5e3c07892a270d78d9fc05ff099d132049
jun097kim <jun097kim@gmail.com>
2018-10-23 11:07:45 +0900

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 cd deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
-/usr/local/bin/docker-compose -f docker-compose-deploy.yaml down
-docker rm -f express
-docker rm -f nginx
-/usr/local/bin/docker-compose -f docker-compose-deploy.yaml up -d
+/usr/local/bin/docker-compose -f down
+/usr/local/bin/docker-compose -f up -d
