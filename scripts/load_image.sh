c6f0e6958869ba5f49b8f7feb83dc15898090d5f
jun097kim <jun097kim@gmail.com>
2018-10-31 22:58:39 +0900

    도커 이미지 로드 전 이미지 및 컨테이너 삭제

--- scripts/load_image.sh
+++ scripts/load_image.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cd deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
+docker rmi -f blockon_express
+docker rmi -f blockon_nginx
 docker load -i blockon_express.tar
 docker load -i blockon_nginx.tar
