15625b9e52882ba8b2562361c5ec0d69c8beff3f
jun097kim <jun097kim@gmail.com>
2018-11-03 01:20:39 +0900

    [거래목록 페이지] 변경된 시안 적용

--- blockon-frontend/src/components/contract/ContractCard/ContractCard.js
+++ blockon-frontend/src/components/contract/ContractCard/ContractCard.js
@@ -1,34 +1,16 @@
 import React from 'react';
+import * as ContractUtils from 'lib/utils/contract';
 import maemulImage from 'static/images/maemul.png';
-import { Badge } from 'antd';
 import './ContractCard.scss';
 
-// 계약상태 상수
-const state = Object.freeze({
-  DOWN_PAYMENT: 1, // 계약금 입금
-  MIDDLE_PAYMENT: 2, // 중도금 입금
-  FINAL_PAYMENT: 3, // 잔금 입금
-  REGISTRATION: 4, // 등기 등록 신청
-  FIXED_DATE: 5, // 확정일자
-  COMPLETED_CONTRACT: 100 // 완료
-});
+const { cs, getStepWord, getKoreanBuildingType } = ContractUtils;
 
-const getStepWord = step => {
-  switch (step) {
-  case state.DOWN_PAYMENT:
-    return '계약금 처리 진행 중';
-  case state.MIDDLE_PAYMENT:
-    return '중도금 처리 진행 중';
-  case state.FINAL_PAYMENT:
-    return '잔금 처리 진행 중';
-  case state.REGISTRATION:
-    return '등기신청 진행 중';
-  case state.FIXED_DATE:
-    return '확정일자 신청 진행 중';
-  case state.COMPLETED_CONTRACT:
-    return '거래 종료';
-  default:
-  }
+const StepBadge = ({ children }) => {
+  return <span className="StepBadge">{children}</span>;
+};
+
+const BuildingTypeBadge = ({ children }) => {
+  return <span className="BuildingTypeBadge">{children}</span>;
 };
 
 const getCards = (contractInfoList, activeTab) => {
@@ -36,12 +18,11 @@ const getCards = (contractInfoList, activeTab) => {
     const { building, state: contractState } = contractInfo;
 
     if (
-      (activeTab === 'ongoing' && contractState !== state.COMPLETED_CONTRACT) ||
-      (activeTab === 'completed' && contractState === state.COMPLETED_CONTRACT)
+      (activeTab === 'ongoing' && contractState !== cs.COMPLETED_CONTRACT) ||
+      (activeTab === 'completed' && contractState === cs.COMPLETED_CONTRACT)
     ) {
       return (
         <div className="card" key={index}>
-          <p className="no">No. {building._id.slice(0, 6)}</p>
           <div className="content">
             <div className="image">
               {building.photo ? (
@@ -54,21 +35,22 @@ const getCards = (contractInfoList, activeTab) => {
               ) : (
                 <img src={maemulImage} alt="maemul" />
               )}
+              <StepBadge>{getStepWord(contractState)}</StepBadge>
             </div>
             <div className="detail">
-              <p>18.07.05</p>
-              <p>{building.name}</p>
-              <p>{building.address}</p>
-              <p>
-                <Badge
-                  status={
-                    contractState !== state.COMPLETED_CONTRACT
-                      ? 'processing'
-                      : 'success'
-                  }
-                  text={getStepWord(contractState)}
-                />
+              <BuildingTypeBadge>
+                {getKoreanBuildingType(building.type)}
+              </BuildingTypeBadge>
+              <p className="building">{building.name}</p>
+              <p className="info">
+                <span>거래시작</span>
+                18.07.05
               </p>
+              <p className="info">
+                <span>위치</span>
+                {building.address}
+              </p>
+              <p className="view">상세보기</p>
             </div>
           </div>
         </div>
--- blockon-frontend/src/components/contract/ContractList/ContractList.js
+++ blockon-frontend/src/components/contract/ContractList/ContractList.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import Chat from '../Chat';
+import * as ContractUtils from 'lib/utils/contract';
 import maemulImage from 'static/images/maemul.png';
 import { Button } from 'antd';
-import Chat from '../Chat';
 import './ContractList.scss';
 
-// 계약종류 상수
-const type = Object.freeze({
-  WOLSE: 1,
-  JEONSE: 2,
-  TRADE: 3
-});
-
-// 계약상태 상수
-const state = Object.freeze({
-  START_TRADE: 0, // 거래시작
-  DOWN_PAYMENT: 1, // 계약금 입금
-  MIDDLE_PAYMENT: 2, // 중도금 입금
-  FINAL_PAYMENT: 3, // 잔금 입금
-  REGISTRATION: 4, // 등기 등록 신청
-  FIXED_DATE: 5, // 확정일자
-  END_TRADE: 100 // 거래종료
-});
+const {
+  ct,
+  cs,
+  tradeStep,
+  rentStep,
+  getStepWord,
+  getKoreanBuildingType
+} = ContractUtils;
 
 // .progressbar > li에 붙는 클래스 이름 상수
 const liClass = Object.freeze({
@@ -50,32 +41,16 @@ class ContractList extends Component {
   getProgressbarItem = (stepIndex, listClassName, cardIndex, contractType) => {
     const { handleSelect, accountInstance, activeTab } = this.props;
 
-    const tradeStep = [
-      state.START_TRADE,
-      state.DOWN_PAYMENT,
-      state.MIDDLE_PAYMENT,
-      state.FINAL_PAYMENT,
-      state.REGISTRATION,
-      state.END_TRADE
-    ];
-    const rentStep = [
-      state.START_TRADE,
-      state.DOWN_PAYMENT,
-      state.FINAL_PAYMENT,
-      state.FIXED_DATE,
-      state.END_TRADE
-    ];
-
     const nextStep = (currentStep => {
       switch (contractType) {
-      case type.TRADE:
+      case ct.TRADE:
         for (const [index, step] of tradeStep.entries()) {
           if (step === currentStep) {
             return tradeStep[index + 1];
           }
         }
         break;
-      case type.RENT:
+      case ct.RENT:
         for (const [index, step] of rentStep.entries()) {
           if (step === currentStep) {
             return rentStep[index + 1];
@@ -85,26 +60,6 @@ class ContractList extends Component {
       default:
       }
     })(stepIndex);
-
-    const getStepWord = step => {
-      switch (step) {
-      case state.START_TRADE:
-        return '거래시작';
-      case state.DOWN_PAYMENT:
-        return '계약금';
-      case state.MIDDLE_PAYMENT:
-        return '중도금';
-      case state.FINAL_PAYMENT:
-        return '잔금';
-      case state.REGISTRATION:
-        return '등기신청';
-      case state.FIXED_DATE:
-        return '확정일자';
-      case state.END_TRADE:
-        return '거래종료';
-      default:
-      }
-    };
 
     return (
       <li
@@ -132,23 +87,10 @@ class ContractList extends Component {
   getProgressbarList = (contractType, contractState, cardIndex) => {
     let contractStep;
 
-    if (contractType === type.TRADE) {
-      contractStep = [
-        state.START_TRADE,
-        state.DOWN_PAYMENT,
-        state.MIDDLE_PAYMENT,
-        state.FINAL_PAYMENT,
-        state.REGISTRATION,
-        state.END_TRADE
-      ];
+    if (contractType === ct.TRADE) {
+      contractStep = tradeStep;
     } else {
-      contractStep = [
-        state.START_TRADE,
-        state.DOWN_PAYMENT,
-        state.FINAL_PAYMENT,
-        state.FIXED_DATE,
-        state.END_TRADE
-      ];
+      contractStep = rentStep;
     }
 
     const progressbarList = [];
@@ -186,26 +128,15 @@ class ContractList extends Component {
     const { index, type: contractType, state, people, building } = contractInfo;
 
     let contractData;
-    if (contractType === type.WOLSE) {
+    if (contractType === ct.WOLSE) {
       contractData = <p>월세 1,000/45</p>;
     }
-    if (contractType === type.JEONSE) {
+    if (contractType === ct.JEONSE) {
       contractData = <p>전세 5,000</p>;
     }
-    if (contractType === type.TRADE) {
+    if (contractType === ct.TRADE) {
       contractData = <p>매매 10억</p>;
     }
-
-    const getKoreanBuildingType = eng => {
-      const map = {
-        jutaek: '주택',
-        apartment: '아파트',
-        sangga: '상가',
-        officetel: '오피스텔'
-      };
-
-      return map[eng];
-    };
 
     const card = (
       <div className="card" key={index}>
@@ -234,7 +165,7 @@ class ContractList extends Component {
         </div>
         <div className="progressbar-wrapper">
           <ul className="progressbar">
-            {this.getProgressbarList(type, state, index)}
+            {this.getProgressbarList(contractType, state, index)}
           </ul>
         </div>
         <Button onClick={() => this.toggleChat(people)}>채팅</Button>
@@ -252,8 +183,8 @@ class ContractList extends Component {
       const { state: contractState } = contractInfo;
 
       if (
-        (activeTab === 'ongoing' && contractState !== state.END_TRADE) ||
-        (activeTab === 'completed' && contractState === state.END_TRADE)
+        (activeTab === 'ongoing' && contractState !== cs.END_TRADE) ||
+        (activeTab === 'completed' && contractState === cs.END_TRADE)
       ) {
         cards.push(this.getCard(contractInfo));
       }
@@ -275,12 +206,7 @@ class ContractList extends Component {
     return (
       <div className="ContractList">
         <div className="list-wrapper">{this.getLists()}</div>
-        <div className="sidebar">
-          {showChat && <Chat party={chatParty} />}
-          <button className="upload">
-            <Link to="/contract/edit">계약 올리기</Link>
-          </button>
-        </div>
+        <div>{showChat && <Chat party={chatParty} />}</div>
       </div>
     );
   }
