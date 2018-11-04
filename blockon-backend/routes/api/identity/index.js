9c00ff1dc97fa9981dd08ca2692f828d18083f54
배성훈 <sung10gf@naver.com>
2018-11-05 00:53:30 +0900

    공인중개사 인증 로직 구현
    - 인증을 위한 공공데이터api사용
    - 인증할 데이터를 추출하기 위한 vision api 사용

--- blockon-backend/routes/api/identity/index.js
+++ blockon-backend/routes/api/identity/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-const { uploadIdentity, referenceIdentity,downloadIdentity } = require('./identity.controller');
+const { uploadIdentity, referenceIdentity,downloadIdentity, isRightIdentity, check } = require('./identity.controller');
 
 router.post('/', uploadIdentity);
 router.get('/:ethAddress', referenceIdentity);
 router.get('/download/:ethAddress',downloadIdentity);
+router.get('/check/agent', isRightIdentity);
+router.post('/check/pdf', check);
 
 module.exports = router;
