9392322aedcd0221e97a1d8c62ef798c88b9bdec
jun097kim <jun097kim@gmail.com>
2018-11-10 02:39:24 +0900

    docker-compose 파일 병합

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 docker rm -f blockon_express
 docker rm -f blockon_nginx
-/usr/local/bin/docker-compose -f docker-compose-deploy.yaml down
-/usr/local/bin/docker-compose -f docker-compose-deploy.yaml up -d
+/usr/local/bin/docker-compose down
+/usr/local/bin/docker-compose up -d
