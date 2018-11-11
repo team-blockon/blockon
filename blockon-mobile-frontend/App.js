abf4b6f9184b5b764c4ddab6f0a29804daee85c2
himchan kim <kimhim7025@gmail.com>
2018-11-11 14:01:44 +0900

    mobile frontend update
    안드로이드 status bar가 앱화면과 겹치는 문제 해결 - ios쪽은 설정은 했으나 확인해봐야함

--- blockon-mobile-frontend/App.js
+++ blockon-mobile-frontend/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import { WebView } from 'react-native';
+import React, { Component } from "react";
+import { WebView, StyleSheet, Platform, StatusBar } from "react-native";
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
-    return <WebView source={{uri: 'http://52.79.254.194'}} />
+    return (
+      <WebView
+        source={{ uri: "http://52.79.254.194" }}
+        style={styles.app}
+        useWebKit={Platform.OS === "ios" ? true : false}
+      />
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  app: {
+    marginTop: Platform.OS === "ios" ? 18 : StatusBar.currentHeight
+  }
+});
