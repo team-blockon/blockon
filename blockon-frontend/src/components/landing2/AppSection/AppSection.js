d7c8674693315b3b3b9942332cd5b101fdcfc9fe
jun097kim <jun097kim@gmail.com>
2018-11-11 21:13:38 +0900

    [랜딩페이지] 헤더 및 푸터 변경된 시안 적용

--- blockon-frontend/src/components/landing2/AppSection/AppSection.js
+++ blockon-frontend/src/components/landing2/AppSection/AppSection.js
@@ -1,32 +1,24 @@
 import React from 'react';
-import blockonIcon from 'static/images/icon/blockon-icon.svg'
-import checkIcon from 'static/images/icon/check.png'
+import blockonIcon from 'static/images/icon/blockon-icon.svg';
+import checkIcon from 'static/images/icon/check.png';
 import './AppSection.scss';
 
-
 const AppSection = () => {
-
-    return(
-        <section className="AppSection">
-            <div>
-                <img src={blockonIcon} alt="blockon" />
-            </div>
-            <p>
-                BLOCKON 앱 다운받기
-            </p>
-            <p>
-                스마트폰에서 더욱 편리하게 사용하세요.
-            </p>
-            <div className="phoneInput">
-                <input
-                    placeholder="010-xxxx-xxxx"
-                />
-                <div>
-                    <img src={checkIcon} alt="confirm" />
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className="AppSection">
+      <div>
+        <img src={blockonIcon} alt="blockon" />
+      </div>
+      <p>BLOCKON 앱 다운받기</p>
+      <p>스마트폰에서 더욱 편리하게 사용하세요.</p>
+      <div className="phoneInput">
+        <input placeholder="010-xxxx-xxxx" />
+        <div>
+          <img src={checkIcon} alt="confirm" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AppSection;
