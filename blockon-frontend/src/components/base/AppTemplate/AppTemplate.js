2e92a805f0f06c73840716aac76f802b4fc46d52
jun097kim <jun097kim@gmail.com>
2018-11-12 22:18:27 +0900

    로그인/회원가입 페이지 반응형 적용

--- blockon-frontend/src/components/base/AppTemplate/AppTemplate.js
+++ blockon-frontend/src/components/base/AppTemplate/AppTemplate.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import classNames from 'classnames';
 import Footer from 'components/base/Footer';
 import './AppTemplate.scss';
 
@@ -7,7 +8,13 @@ const AppTemplate = ({ header, children, location }) => {
   return (
     <div className="AppTemplate">
       {header}
-      <main>{children}</main>
+      <main
+        className={classNames({
+          auth: location.pathname.startsWith('/auth')
+        })}
+      >
+        {children}
+      </main>
       {location.pathname !== '/search' && <Footer />}
     </div>
   );
