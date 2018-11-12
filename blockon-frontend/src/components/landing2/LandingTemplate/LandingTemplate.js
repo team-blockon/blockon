44d2d64105138d6873ca4977155e8f1c67f2e190
jun097kim <jun097kim@gmail.com>
2018-11-12 20:39:20 +0900

    [랜딩페이지] 자동완성 컴포넌트 변경

--- blockon-frontend/src/components/landing2/LandingTemplate/LandingTemplate.js
+++ blockon-frontend/src/components/landing2/LandingTemplate/LandingTemplate.js
@@ -1,50 +1,15 @@
 import React, { Component, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
-import * as MapAPI from 'lib/api/map';
 
 import CoverSection from '../CoverSection';
 import BlockonSection from '../BlockonSection';
 import AppSection from '../AppSection';
 
-const searchResult = value => {
-  return new Promise((resolve, reject) => {
-    MapAPI.getSubways(value).then(res => {
-      const subways = res.data.documents;
-      resolve(subways.map(document => document.place_name));
-    });
-  });
-};
-
 class LandingTemplate extends Component {
-  state = {
-    dataSource: [],
-    agent: ''
-  };
-
-  handleSearch = async value => {
-    const subways = await searchResult(value);
-
-    if (subways.length > 0) {
-      this.setState({ dataSource: subways });
-    }
-  };
-
-  handlePressEnter = event => {
-    const { history } = this.props;
-    history.push(`/search?q=${event.target.value}`);
-  };
-
   render() {
-    const { dataSource, agent } = this.state;
-
     return (
       <Fragment>
-        <CoverSection
-          dataSource={dataSource}
-          agent={agent}
-          handleSearch={this.handleSearch}
-          handlePressEnter={this.handlePressEnter}
-        />
+        <CoverSection />
         <BlockonSection />
         <AppSection />
       </Fragment>
